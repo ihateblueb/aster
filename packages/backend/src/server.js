42c4a40f0c11a8bbde7e49f3dcd594f8e50ee530
blueb <ihateblueb@proton.me>
2024-04-25 19:26:07 -0400

    did a little tweaking

--- packages/backend/src/server.js
+++ packages/backend/src/server.js
@@ -1,34 +1,29 @@
-// Patches
 const {inject, errorHandler} = require('express-custom-error');
-inject(); // Patch express in order to use async / await syntax
+inject();
 
-// Require Dependencies
-
+const yaml = require('js-yaml');
+const fs = require('fs');
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const helmet = require('helmet');
-
-
 const logger = require('./util/logger.js');
 
-// Load .env Enviroment Variables to process.env
+console.log("[config] loading configuration...");
 
-require('mandatoryenv').load([
-    'DB_HOST',
-    'DB_DATABASE',
-    'DB_USER',
-    'DB_PASSWORD',
-    'PORT',
-    'SECRET'
-]);
+try {
+    var config = yaml.load(fs.readFileSync('../../.config/production.yml', 'utf8'));
+    console.log("[config] configuration loaded successfully!");
+} catch (e) {
+    console.error("[config] "+e);
+    console.error("[config] fatal. now aborting.");
+    process.exit(1);
+}
 
-const { PORT } = process.env;
-
+const port = config.port;
 
 // Instantiate an Express Application
 const app = express();
-
 
 // Configure Express App Instance
 app.use(express.json( { limit: '50mb' } ));
@@ -48,9 +43,7 @@ app.use('*', (req, res, next) => {
 })
 
 // Assign Routes
-
 app.use('/', require('./routes/router.js'));
-
 
 // Handle errors
 app.use(errorHandler());
@@ -59,11 +52,10 @@ app.use(errorHandler());
 app.use('*', (req, res) => {
     res
     .status(404)
-    .json( {status: false, message: 'Endpoint Not Found'} );
+    .json( {status: false, message: 'Not found.'} );
 })
 
-// Open Server on selected Port
 app.listen(
-    PORT,
-    () => console.info('Server listening on port ', PORT)
+    port,
+    () => console.info(`[backend] listening on port ${port}`)
 );
