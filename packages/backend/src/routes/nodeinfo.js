266b0cd89a9babbdb8c1167d27488ef4b7d20878
blueb <ihateblueb@proton.me>
2024-04-27 14:12:20 -0400

    add editor config

--- packages/backend/src/routes/nodeinfo.js
+++ packages/backend/src/routes/nodeinfo.js
@@ -5,22 +5,22 @@ const pkg = require('../../../../package.json')
 const config = require('../util/config.js')
 
 router.get('/nodeinfo/2.0', (req, res) => {
-    res.json({
-        version: '2.0',
-        software: {
-            name: `${pkg.name}`,
-            version: `${pkg.version}`
-        },
-        protocols: ['activitypub'],
-        services: {
-            outbound: [],
-            inbound: []
-        },
-        openRegistrations: false,
-        metadata: {
-            nodeName: `${config.nodename}`
-        }
-    })
+	res.json({
+		version: '2.0',
+		software: {
+			name: `${pkg.name}`,
+			version: `${pkg.version}`
+		},
+		protocols: ['activitypub'],
+		services: {
+			outbound: [],
+			inbound: []
+		},
+		openRegistrations: false,
+		metadata: {
+			nodeName: `${config.nodename}`
+		}
+	})
 })
 
 module.exports = router
