9a6985bd06c07aee413cb1587a20e73ac0ba0b0f
blueb <ihateblueb@proton.me>
2024-04-28 00:16:31 -0400

    working on user inbox stuff

--- packages/backend/src/routes/user.js
+++ packages/backend/src/routes/user.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const crypto = require('crypto');
 
 const config = require('../util/config.js');
 const db = require('../util/database.ts');
@@ -32,9 +33,11 @@ router.get('/users/:userid', async (req, res) => {
 			if (grabbedUser.displayname) {
 				userJson['name'] = grabbedUser.displayname;
 			}
+
 			if (grabbedUser.bio) {
 				userJson['summary'] = grabbedUser.bio;
 			}
+
 			if (grabbedUser.avatar) {
 				userJson['icon'] = {};
 				userJson.icon['type'] = 'Image';
@@ -67,6 +70,58 @@ router.get('/users/:userid', async (req, res) => {
 			res.json(userJson);
 		} else {
 			return res.status(404).send('Not found');
+		}
+	}
+});
+
+router.post('/users/:userid/inbox', async (req, res) => {
+	if (!req.params.userid) {
+		return res.status(400).send('Bad request');
+	} else {
+		var grabbedUser = await db.getRepository('users').find({
+			where: {
+				id: Number(req.params.userid)
+			}
+		});
+
+		var grabbedUser = grabbedUser[0];
+
+		var host = req.headers.host;
+		var date = req.headers.date;
+		var digest = req.headers.digest;
+		var signature = req.headers.signature;
+
+		console.log(host);
+		console.log(date);
+		console.log(digest);
+		console.log(signature);
+
+		// needs more validation!
+		// see: https://github.com/misskey-dev/misskey/blob/develop/packages/backend/src/server/ActivityPubServerService.ts
+		if (
+			!host ||
+			host !==
+				config.url
+					.replace('https://', '')
+					.replace('http://', '')
+					.replace('/', '')
+		) {
+			console.log(
+				'[ap] uh-oh! a request was sent that mismatches with the current host'
+			);
+		} else {
+			if (grabbedUser) {
+				if (!grabbedUser.followerapproval) {
+					// send!
+					var followApprovalResponse = {
+						'@context': 'https://www.w3.org/ns/activitystreams'
+					};
+				} else {
+					// wait.
+				}
+			} else {
+				return res.status(404).send('Not found');
+			}
 		}
 	}
 });
