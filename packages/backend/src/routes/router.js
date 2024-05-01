9418dea5f5673cca2201c5b90738f23dad4d5a71
blueb <ihateblueb@proton.me>
2024-05-01 02:32:54 -0400

    add comments to router

--- packages/backend/src/routes/router.js
+++ packages/backend/src/routes/router.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const router = require('express').Router();
 
+// eventually will point towards static generated frontend
 router.use('/', express.static('../../frontend/.output/public'));
 router.use('/', require('./misc.js'));
 
+// required endpoints for federation
 router.use('/', require('./ap/well-known.js'));
 router.use('/', require('./ap/nodeinfo.js'));
 router.use('/', require('./ap/inbox.js'));
 router.use('/', require('./ap/user.js'));
 router.use('/', require('./ap/note.js'));
 
+// api
 router.use('/', require('./api/v1/accounts.js'));
 
 module.exports = router;
