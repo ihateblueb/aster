45f176d6f997ef0a8d017938cc385a8628b56e9e
blueb <ihateblueb@proton.me>
2024-05-15 10:14:16 -0400

    processing seems good for mfm, now its just styling problems

--- packages/frontend/src/lib/components/Mfm.svelte
+++ packages/frontend/src/lib/components/Mfm.svelte
@@ -13,31 +13,43 @@
 	}
 
 	/*
-        elements todo:
-        - text
-		- url
-        - bold
-        - italic
-        - strikethrough
-        - quote
+		elements done:
+		- text
+		- bold
+		- italic
+		- strike
 		- plain
 		- center
 		- small
-        - fn spin
-        - fn tada
-        - fn jump
-        - fn bounce
-        - fn twitch
-        - fn shake
-        - fn rainbow
-        - fn x2
-        - fn x3
-        - fn x4
+		- url
+		- link
+		- blockquote
+		- fn tada
+		- fn jelly (animation needed)
+		- fn twitch
+		- fn shake
+		- fn spin
+		- fn jump
+		- fn bounce
+		- fn flip
+		- fn x2
+		- fn x3
+		- fn x4
+		- fn font
+		- fn blur
+		- fn rainbow
+		- fn sparkle (does nothing rn but handled)
+		- fn rotate
+		- fn position
+		- fn scale
+		- fn fg
+		- fn bg
+		- fn border
 		- fn plain
 		- fn center
 		- fn small
-		- fn blur
 
+        elements todo:
 		- hashtag
 		- mention
 		- math
@@ -46,44 +58,163 @@
 		- inline code
 		- emoji
 		- search
-		- flip
-		- fg
-		- bg
-		- font
-		- rotate
-		- jelly
-		- sparkle
-		- position
-		- scale
-		- additional options (speed, x/y, alternate, etc.)
+		- ruby
 
         known issues:
         they dont stack.
+		animations dont work although they are applied.
     */
 
-	function renderEachChild(objChild) {
+	function renderEachChild(objChild, scale) {
 		let collectedChildren = '';
 		objChild.forEach((child) => {
-			collectedChildren = collectedChildren + renderMfm(child);
+			collectedChildren = collectedChildren + renderMfm(child, scale);
 		});
 		return collectedChildren;
 	}
 
-	function renderMfm(object) {
+	function renderMfm(object, scale) {
+		if (!scale) {
+			scale = 1;
+		}
 		if (object.type === 'text') {
 			return object.props.text.replace(/(\r\n|\n|\r)/g, '\n');
 		} else if (object.type === 'bold') {
-			return `<b>${renderEachChild(object.children)}</b>`;
+			return `<b style="display: inline-block;">${renderEachChild(object.children, scale)}</b>`;
 		} else if (object.type === 'strike') {
-			return `<s>${renderEachChild(object.children)}</ss>`;
+			return `<s style="display: inline-block;">${renderEachChild(object.children, scale)}</ss>`;
 		} else if (object.type === 'italic') {
-			return `<i>${renderEachChild(object.children)}</i>`;
+			return `<i style="display: inline-block;">${renderEachChild(object.children, scale)}</i>`;
+		} else if (object.type === 'plain') {
+			return `<span style="display: inline-block;">${renderEachChild(object.children, scale)}</span>`;
+		} else if (object.type === 'small') {
+			return `<small style="opacity: 75%;">${renderEachChild(object.children, scale)}</small>`;
+		} else if (object.type === 'center') {
+			return `<span style="text-align: center;">${renderEachChild(object.children, scale)}</span>`;
+		} else if (object.type === 'url') {
+			return `<a href="${object.props.url}" rel="nofollow noopener">${object.props.url}</a>`;
+		} else if (object.type === 'link') {
+			return `<a href="${object.props.url}" rel="nofollow noopener">${renderEachChild(object.children, scale)}</a> <small style="font-size: 75%; color: var(--txt-tertiary);">(${object.props.url})</small>`;
+		} else if (object.type === 'quote') {
+			return `<blockquote class="mfm-quote">${renderEachChild(object.children, scale)}</blockquote>`;
 		} else if (object.type === 'fn') {
 			if (object.props.name === 'tada') {
 				let speed = object.props.args.speed || '1s';
 				let delay = object.props.args.delay || '0s';
-				return `<span style="font-size: 150%; animation: ${speed} linear ${delay} infinite normal both running mfm-tada;">${renderEachChild(object.children)}</span>`;
+				return `<span style="display: inline-block; animation: mfm-tada ${speed} linear infinite; animation-delay: ${delay}; font-size: 150%;">${renderEachChild(object.children, scale)}</span>`;
+			} else if (object.props.name === 'jelly') {
+				let speed = object.props.args.speed || '1s';
+				let delay = object.props.args.delay || '0s';
+				return `<span style="display: inline-block; animation: mfm-jel;y ${speed} linear infinite; animation-delay: ${delay};">${renderEachChild(object.children, scale)}</span>`;
+			} else if (object.props.name === 'twitch') {
+				let speed = object.props.args.speed || '1s';
+				let delay = object.props.args.delay || '0s';
+				return `<span style="display: inline-block; animation: mfm-twitch ${speed} linear infinite; animation-delay: ${delay};">${renderEachChild(object.children, scale)}</span>`;
+			} else if (object.props.name === 'shake') {
+				let speed = object.props.args.speed || '1s';
+				let delay = object.props.args.delay || '0s';
+				return `<span style="display: inline-block; animation: mfm-shake ${speed} linear infinite; animation-delay: ${delay};">${renderEachChild(object.children, scale)}</span>`;
+			} else if (object.props.name === 'spin') {
+				let direction = object.props.args.left
+					? 'reverse'
+					: object.props.args.alternate
+						? 'alternate'
+						: 'normal';
+				let animation = object.props.args.x
+					? 'mfm-spinX'
+					: object.props.args.y
+						? 'mfm-spinY'
+						: 'mfm-spin';
+				let speed = object.props.args.speed || '1.5s';
+				let delay = object.props.args.delay || '0s';
+				return `<span style="display: inline-block; animation: ${animation} ${speed} linear infinite; animation-delay: ${delay}; animation-direction: ${direction};">${renderEachChild(object.children, scale)}</span>`;
+			} else if (object.props.name === 'jump') {
+				let speed = object.props.args.speed || '1s';
+				let delay = object.props.args.delay || '0s';
+				return `<span style="display: inline-block; animation: mfm-jump ${speed} linear infinite; animation-delay: ${delay};">${renderEachChild(object.children, scale)}</span>`;
+			} else if (object.props.name === 'bounce') {
+				let speed = object.props.args.speed || '1s';
+				let delay = object.props.args.delay || '0s';
+				return `<span style="display: inline-block; animation: mfm-bounce ${speed} linear infinite; animation-delay: ${delay};">${renderEachChild(object.children, scale)}</span>`;
+			} else if (object.props.name === 'flip') {
+				let transform =
+					object.props.args.h && object.props.args.v
+						? 'scale(-1,-1)'
+						: object.props.args.v
+							? 'scaleY(-1)'
+							: 'scaleX(-1)';
+				return `<span style="display: inline-block; transform: ${transform}">${renderEachChild(object.children, scale)}</span>`;
+			} else if (object.props.name === 'x2') {
+				scale = scale * 2;
+				return `<span class="mfm-x2" style="display: inline-block;">${renderEachChild(object.children, scale)}</span>`;
+			} else if (object.props.name === 'x3') {
+				scale = scale * 3;
+				return `<span class="mfm-x3" style="display: inline-block;">${renderEachChild(object.children, scale)}</span>`;
+			} else if (object.props.name === 'x4') {
+				scale = scale * 4;
+				return `<span class="mfm-x4" style="display: inline-block;">${renderEachChild(object.children, scale)}</span>`;
+			} else if (object.props.name === 'font') {
+				let font = object.props.args.serif
+					? 'serif'
+					: object.props.args.monospace
+						? 'monospace'
+						: object.props.args.cursive
+							? 'cursive'
+							: object.props.args.fantasy
+								? 'fantasy'
+								: object.props.args.emoji
+									? 'emoji'
+									: object.props.args.math
+										? 'math'
+										: null;
+				return `<span style="display: inline-block; font-family: ${font};">${renderEachChild(object.children, scale)}</span>`;
+			} else if (object.props.name === 'blur') {
+				return `<span class="mfm-blur" style="display: inline-block;">${renderEachChild(object.children, scale)}</span>`;
+			} else if (object.props.name === 'rainbow') {
+				let speed = object.props.args.speed || '1s';
+				let delay = object.props.args.delay || '0s';
+				return `<span style="display: inline-block; animation: mfm-rainbow ${speed} linear infinite; animation-delay: ${delay};">${renderEachChild(object.children, scale)}</span>`;
+			} else if (object.props.name === 'sparkle') {
+				// deal with this later
+				return renderEachChild(object.children, scale);
+			} else if (object.props.name === 'rotate') {
+				let degrees = object.props.args.deg || '90';
+				return `<span style="display: inline-block; transform: rotate(${degrees}deg); transform-origin: center center;">${renderEachChild(object.children, scale)}</span>`;
+			} else if (object.props.name === 'position') {
+				let x = object.props.args.x || 0;
+				let y = object.props.args.y || 0;
+				return `<span style="display: inline-block; transform: translateX(${x}em) translateY(${y}em);">${renderEachChild(object.children, scale)}</span>`;
+			} else if (object.props.name === 'scale') {
+				let x = Math.min(object.props.args.x) || 1;
+				let y = Math.min(object.props.args.y) || 1;
+				scale = scale * Math.max(x, y);
+				return `<span style="display: inline-block; transform: scale(${x}, ${y});">${renderEachChild(object.children, scale)}</span>`;
+			} else if (object.props.name === 'fg') {
+				let color = object.props.args.color || 'f00';
+				return `<span style="display: inline-block; color: #${color};">${renderEachChild(object.children, scale)}</span>`;
+			} else if (object.props.name === 'bg') {
+				let color = object.props.args.color || 'f00';
+				return `<span style="display: inline-block; background-color: #${color};">${renderEachChild(object.children, scale)}</span>`;
+			} else if (object.props.name === 'border') {
+				let color = object.props.args.color || 'var(--accent)';
+				let style = object.props.args.style || 'solid';
+				let width = object.props.args.width || 1;
+				let radius = object.props.args.radius || 0;
+				let noclip = object.props.args.noclip ? '' : ' ovrflow: clip;';
+				return `<span style="display: inline-block; border: ${width}px ${style} ${color}; border-raidus: ${radius};${noclip}">${renderEachChild(object.children, scale)}</span>`;
+			} else if (object.props.name === 'plain') {
+				return `<span style="display: inline-block;">${renderEachChild(object.children, scale)}</span>`;
+			} else if (object.props.name === 'small') {
+				return `<small style="opacity: 75%;">${renderEachChild(object.children, scale)}</small>`;
+			} else if (object.props.name === 'center') {
+				return `<span style="text-align: center;">${renderEachChild(object.children, scale)}</span>`;
+			} else {
+				// if the element is unknown
+				return `<span style="display: inline-block;">${renderEachChild(object.children, scale)}</span>`;
 			}
+		} else {
+			// if the element is unknown
+			return `<span style="display: inline-block;">${renderEachChild(object.children, scale)}</span>`;
 		}
 	}
 
@@ -95,13 +226,38 @@
 		{@html renderMfm(object)}
 	{/each}
 </span>
-<br />
-<p>
-	Raw MFM Tree:
-	<code>{JSON.stringify(mfmTree)}</code>
-</p>
 
 <style lang="scss">
+	.mfmCtn {
+		display: block;
+		overflow-wrap: break-word;
+		position: relative;
+		white-space: preserve;
+	}
+
+	.mfm-quote {
+		margin: 4px 12px;
+		color: var(--txt-tertiary);
+		padding-left: 8px;
+		border-left: 2px solid var(--txt-tertiary);
+	}
+
+	.mfm-rainbow {
+		background-image: linear-gradient(
+			to right,
+			rgb(255, 0, 0) 0%,
+			rgb(255, 165, 0) 17%,
+			rgb(255, 255, 0) 33%,
+			rgb(0, 255, 0) 50%,
+			rgb(0, 255, 255) 67%,
+			rgb(0, 0, 255) 83%,
+			rgb(255, 0, 255) 100%
+		);
+		-webkit-background-clip: text;
+		background-clip: text;
+		color: transparent;
+	}
+
 	/* animations taken from misskey's style.scss */
 
 	@keyframes mfm-spin {
@@ -307,104 +463,5 @@
 		100% {
 			transform: translate(2px, 1px) rotate(2deg);
 		}
-	}
-
-	.mfmCtn {
-		display: block;
-		overflow-wrap: break-word;
-		position: relative;
-		white-space: preserve;
-
-		> * {
-			white-space: preserve;
-		}
-	}
-
-	.mfm-blur {
-		filter: blur(6px);
-		transition: 0.1s;
-		&:hover {
-			filter: blur(0px);
-		}
-	}
-
-	.mfm-small {
-		color: var(--txt-tertiary);
-	}
-
-	.mfm-center {
-		display: block;
-		width: 100%;
-		text-align: center;
-	}
-
-	.mfm-plain {
-		display: inline-block;
-	}
-
-	.mfm-rainbow {
-		background-image: linear-gradient(
-			to right,
-			rgb(255, 0, 0) 0%,
-			rgb(255, 165, 0) 17%,
-			rgb(255, 255, 0) 33%,
-			rgb(0, 255, 0) 50%,
-			rgb(0, 255, 255) 67%,
-			rgb(0, 0, 255) 83%,
-			rgb(255, 0, 255) 100%
-		);
-		-webkit-background-clip: text;
-		background-clip: text;
-		color: transparent;
-	}
-
-	.mfm-jump {
-		display: inline-block;
-		animation: 1s linear 0s infinite normal both running mfm-jump;
-	}
-
-	.mfm-bounce {
-		display: inline-block;
-		animation: 1s linear 0s infinite normal both running mfm-bounce;
-	}
-
-	.mfm-twitch {
-		display: inline-block;
-		animation: 1s linear 0s infinite normal both running mfm-twitch;
-	}
-
-	.mfm-shake {
-		display: inline-block;
-		animation: 1s linear 0s infinite normal both running mfm-shake;
-	}
-
-	.mfm-tada {
-		display: inline-block;
-		font-size: 150%;
-		animation: 1s linear 0s infinite normal both running mfm-tada;
-	}
-
-	.mfm-spin {
-		display: inline-block;
-		animation: 1s linear 0s infinite normal none running mfm-spin;
-	}
-
-	.mfm-x2 {
-		font-size: 200%;
-	}
-
-	.mfm-x3 {
-		font-size: 300%;
-	}
-
-	.mfm-x4 {
-		font-size: 400%;
-	}
-
-	blockquote {
-		margin: 4px 12px;
-		color: var(--txt-tertiary);
-		padding-left: 8px;
-		border-left: 2px solid var(--txt-tertiary);
 	}
 </style>
