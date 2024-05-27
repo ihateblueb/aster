ef3bc41e590287aecf732d6d10d0dd0bc5db28a3
blueb <ihateblueb@proton.me>
2024-05-27 13:33:10 -0400

    fix formatting script

--- packages/backend/src/migrations/1716684142544-UserAuthChangeExpiresToCreatedAt.ts
+++ packages/backend/src/migrations/1716684142544-UserAuthChangeExpiresToCreatedAt.ts
@@ -1,14 +1,17 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class AddUserAuth1716684142544 implements MigrationInterface {
-    name = 'AddUserAuth1716684142544'
+	name = 'AddUserAuth1716684142544';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "users_auth" RENAME COLUMN "expires" TO "created_at"`);
-    }
+	public async up(queryRunner: QueryRunner): Promise<void> {
+		await queryRunner.query(
+			`ALTER TABLE "users_auth" RENAME COLUMN "expires" TO "created_at"`
+		);
+	}
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "users_auth" RENAME COLUMN "created_at" TO "expires"`);
-    }
-
+	public async down(queryRunner: QueryRunner): Promise<void> {
+		await queryRunner.query(
+			`ALTER TABLE "users_auth" RENAME COLUMN "created_at" TO "expires"`
+		);
+	}
 }
