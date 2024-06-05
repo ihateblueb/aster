82bc431ca14ff37c326ae3c6534af3457c125351
blueb <ihateblueb@proton.me>
2024-06-05 12:08:00 -0400

    format & add ini formatter & properly ignore public.pem and private.pem

--- packages/backend/src/migrations/1717540153510-AddPinnedNotes.ts
+++ packages/backend/src/migrations/1717540153510-AddPinnedNotes.ts
@@ -1,14 +1,17 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class AddPinnedNotes1717540153510 implements MigrationInterface {
-    name = 'AddPinnedNotes1717540153510'
+	name = 'AddPinnedNotes1717540153510';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "users" ADD "pinned_notes" text array`);
-    }
+	public async up(queryRunner: QueryRunner): Promise<void> {
+		await queryRunner.query(
+			`ALTER TABLE "users" ADD "pinned_notes" text array`
+		);
+	}
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "pinned_notes"`);
-    }
-
+	public async down(queryRunner: QueryRunner): Promise<void> {
+		await queryRunner.query(
+			`ALTER TABLE "users" DROP COLUMN "pinned_notes"`
+		);
+	}
 }
