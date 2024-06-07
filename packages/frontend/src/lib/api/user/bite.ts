6963884594fbe96160db4ec948e5a3f367cfdb89
blueb <ihateblueb@proton.me>
2024-06-07 18:35:44 -0400

    add user ts stuff

---
+++ packages/frontend/src/lib/api/user/bite.ts
@@ -0,0 +1,24 @@
+import Store from '$lib/scripts/Store';
+
+export default async function userBite(userId: String) {
+	let userRes = {};
+
+	var userReq = await fetch(`/api/v1/user/${userId}/bite`, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+			Accept: 'application/json',
+			Authorization: `Bearer ${Store.get('a_token')}`
+		}
+	});
+
+	userRes = await userReq.json();
+
+	if (userReq.status === 200) {
+		console.log(userRes);
+	} else {
+		console.log(userRes);
+	}
+
+	return userRes;
+}
---
+++ packages/frontend/src/lib/api/user/report.ts
@@ -0,0 +1,30 @@
+import Store from '$lib/scripts/Store';
+
+export default async function userReport(
+	userId: String,
+	reportContent: String
+) {
+	let userRes = {};
+
+	var userReq = await fetch(`/api/v1/user/${userId}/report`, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+			Accept: 'application/json',
+			Authorization: `Bearer ${Store.get('a_token')}`
+		},
+		body: JSON.stringify({
+			content: reportContent
+		})
+	});
+
+	userRes = await userReq.json();
+
+	if (userReq.status === 200) {
+		console.log(userRes);
+	} else {
+		console.log(userRes);
+	}
+
+	return userRes;
+}
