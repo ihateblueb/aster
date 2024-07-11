52ca3d440d552f6a116008eae805211baddd994c
blueb <ihateblueb@proton.me>
2024-07-10 22:37:26 -0400

    block localhost hosts through isValidUrl

--- packages/backend/src/utils/isValidUrl.ts
+++ packages/backend/src/utils/isValidUrl.ts
@@ -3,7 +3,22 @@ import logger from './logger.js';
 export default function isValidUrl(url: string) {
 	let newUrl;
 
-	console.log(url);
+	if (url === 'localhost') {
+		logger('error', 'validation', 'stopped request to localhost');
+		return false;
+	} else if (url === '0.0.0.0') {
+		logger('error', 'validation', 'stopped request to 0.0.0.0');
+		return false;
+	} else if (url === '::1') {
+		logger('error', 'validation', 'stopped request to ::1');
+		return false;
+	} else if (url === '127.0.0.1') {
+		logger('error', 'validation', 'stopped request to 127.0.0.1');
+		return false;
+	} else if (url.startsWith('192.168')) {
+		logger('error', 'validation', 'stopped request to 192.168.x.x');
+		return false;
+	}
 
 	try {
 		newUrl = new URL(url);
