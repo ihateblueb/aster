8fed47e53851223824b9bf7e3a84e5a476d3269e
blueb <ihateblueb@proton.me>
2024-07-15 13:22:25 -0400

    update the submodule

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -2,50 +2,11 @@ import { enhancedImages } from '@sveltejs/enhanced-img';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import dynamicImport from 'vite-plugin-dynamic-import';
-import fs from 'fs';
-
-function scssAutoImport() {
-	return {
-		name: 'vite-plugin-svelte-scss-auto-import',
-		load() {
-			return;
-		},
-		transform(code, id) {
-			let pageRegex = /(\+page)\.(svelte)$/;
-			let componentRegex = /((?:[a-zA-Z0-9]+))\.svelte$/;
-
-			if (pageRegex.test(id)) {
-				let scssPath = id.replace('+page.svelte', '+page.scss');
-
-				if (fs.existsSync(scssPath)) {
-					return code + `import './+page.scss';`;
-				} else {
-					return;
-				}
-			} else if (id.match(componentRegex)) {
-				let scssPath = id.replace(
-					id.match(componentRegex)[1] + '.svelte',
-					id.match(componentRegex)[1] + '.scss'
-				);
-
-				if (fs.existsSync(scssPath)) {
-					return (
-						code +
-						`import './${id.match(componentRegex)[1] + '.scss'}';`
-					);
-				} else {
-					return;
-				}
-			} else {
-				return;
-			}
-		}
-	};
-}
+import { autoImportStyle } from 'vite-plugin-svelte-auto-import-style';
 
 export default defineConfig({
 	plugins: [
-		scssAutoImport(),
+		autoImportStyle(),
 		sveltekit(),
 		enhancedImages(),
 		dynamicImport({ loose: true })
