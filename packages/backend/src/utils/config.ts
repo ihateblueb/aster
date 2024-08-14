1a47d2299d88571363a111b7b9d921974ddccc80
blueb <ihateblueb@proton.me>
2024-08-14 13:08:33 -0400

    add eslint for backend

--- packages/backend/src/utils/config.ts
+++ packages/backend/src/utils/config.ts
@@ -1,13 +1,10 @@
-import chalk from 'chalk';
-
 import { parse } from 'ini';
 import { readFile } from 'node:fs/promises';
-import { exit } from 'node:process';
 
 let configText = await readFile(`../../config/production.ini`, {
 	encoding: 'utf-8'
 });
 
-var config = parse(configText);
+let config = parse(configText);
 
 export default config;
