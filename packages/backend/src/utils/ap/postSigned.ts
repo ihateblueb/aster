a2d4ebe2e0742360a728c9b027394568b76fbe2e
blueb <ihateblueb@proton.me>
2024-08-14 00:29:58 -0400

    do not post to myself!!!!

--- packages/backend/src/utils/ap/postSigned.ts
+++ packages/backend/src/utils/ap/postSigned.ts
@@ -7,65 +7,43 @@ import db from '../database.js';
 import logger from '../logger.js';
 
 export default async function postSigned(inbox, localUserId, body) {
-	let grabbedLocalUser = await db.getRepository('user').findOne({
-		where: {
-			id: localUserId
-		}
-	});
-
-	let grabbedLocalUserPriv = await db.getRepository('user_priv').findOne({
-		where: {
-			id: localUserId
-		}
-	});
-
-	const inboxUrl = new URL(inbox);
-
-	const digest = createHash('sha256')
-		.update(JSON.stringify(body))
-		.digest('base64');
-
-	const signer = createSign('sha256');
-	const sendDate = new Date().toISOString();
-
-	const stringToSign = `(request-target): post ${inboxUrl.pathname}\nhost: ${inboxUrl.host}\ndate: ${sendDate}\nalgorithm: rsa-sha256\ndigest: SHA-256=${digest}`;
-	signer.update(stringToSign);
-	signer.end();
-	const signedString = signer
-		.sign(grabbedLocalUserPriv.private_key)
-		.toString('base64');
-
-	const signatureHeader = `keyId="${config.url}users/${grabbedLocalUser.id}#main-key",algorithm="rsa-sha256",headers="(request-target) host date algorithm digest",signature="${signedString}"`;
-
-	console.log('!!OUTGOING POST!!');
-	console.log(inbox);
-	console.log(JSON.stringify(body));
-	console.log({
-		headers: {
-			'Content-Type': 'application/activity+json',
-			'User-Agent': `Aster/${pkg.version}`,
-			Accept: 'application/activity+json',
-			Algorithm: 'rsa-sha256',
-			Host: inboxUrl.host,
-			Date: sendDate,
-			Digest: `SHA-256=${digest}`,
-			Signature: signatureHeader
-		}
-	});
-	console.log('!!OUTGOING POST!!');
-
-	if (config.plugins.outgoing) {
-		await config.plugins.outgoing.forEach(async (e) => {
-			await import(`../../plugins/outgoing/${e}.js`).then(
-				async (plugin) => {
-					await plugin.default();
-				}
-			);
+	if (new URL(inbox).host === new URL(config.url).host) {
+		logger('debug', 'ap', 'not sending post to myself');
+	} else {
+		let grabbedLocalUser = await db.getRepository('user').findOne({
+			where: {
+				id: localUserId
+			}
 		});
-	}
 
-	return await axios
-		.post(inbox, body, {
+		let grabbedLocalUserPriv = await db.getRepository('user_priv').findOne({
+			where: {
+				id: localUserId
+			}
+		});
+
+		const inboxUrl = new URL(inbox);
+
+		const digest = createHash('sha256')
+			.update(JSON.stringify(body))
+			.digest('base64');
+
+		const signer = createSign('sha256');
+		const sendDate = new Date().toISOString();
+
+		const stringToSign = `(request-target): post ${inboxUrl.pathname}\nhost: ${inboxUrl.host}\ndate: ${sendDate}\nalgorithm: rsa-sha256\ndigest: SHA-256=${digest}`;
+		signer.update(stringToSign);
+		signer.end();
+		const signedString = signer
+			.sign(grabbedLocalUserPriv.private_key)
+			.toString('base64');
+
+		const signatureHeader = `keyId="${config.url}users/${grabbedLocalUser.id}#main-key",algorithm="rsa-sha256",headers="(request-target) host date algorithm digest",signature="${signedString}"`;
+
+		console.log('!!OUTGOING POST!!');
+		console.log(inbox);
+		console.log(JSON.stringify(body));
+		console.log({
 			headers: {
 				'Content-Type': 'application/activity+json',
 				'User-Agent': `Aster/${pkg.version}`,
@@ -76,11 +54,41 @@ export default async function postSigned(inbox, localUserId, body) {
 				Digest: `SHA-256=${digest}`,
 				Signature: signatureHeader
 			}
-		})
-		.then(() => {
-			logger('debug', 'ap', 'successfully send a post request signed');
-		})
-		.catch((e) => {
-			logger('error', 'ap', e);
 		});
+		console.log('!!OUTGOING POST!!');
+
+		if (config.plugins.outgoing) {
+			await config.plugins.outgoing.forEach(async (e) => {
+				await import(`../../plugins/outgoing/${e}.js`).then(
+					async (plugin) => {
+						await plugin.default();
+					}
+				);
+			});
+		}
+
+		return await axios
+			.post(inbox, body, {
+				headers: {
+					'Content-Type': 'application/activity+json',
+					'User-Agent': `Aster/${pkg.version}`,
+					Accept: 'application/activity+json',
+					Algorithm: 'rsa-sha256',
+					Host: inboxUrl.host,
+					Date: sendDate,
+					Digest: `SHA-256=${digest}`,
+					Signature: signatureHeader
+				}
+			})
+			.then(() => {
+				logger(
+					'debug',
+					'ap',
+					'successfully send a post request signed'
+				);
+			})
+			.catch((e) => {
+				logger('error', 'ap', e);
+			});
+	}
 }
