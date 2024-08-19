15505a45ba87976b0dda17e1279b0529df2fd82a
blueb <ihateblueb@proton.me>
2024-08-19 14:04:50 -0400

    add contained widget

--- packages/frontend/src/lib/components/widgets/Notifications.svelte
+++ packages/frontend/src/lib/components/widgets/Notifications.svelte
@@ -1,12 +1,22 @@
 <script>
 	import notificationsGet from '$lib/api/notifications/get';
+	import { locale } from '$lib/locale';
+	import Icon from '../Icon.svelte';
 	import Notification from '../Notification.svelte';
 </script>
 
 <template>
-	{#await notificationsGet() then notifications}
-		{#each notifications as notification}
-			<Notification data={notification} small />
-		{/each}
-	{/await}
+	<div class="containedWidget">
+		<div class="header">
+			<Icon name="bell" size="18px" />
+			{locale('notifications')}
+		</div>
+		<div class="content">
+			{#await notificationsGet() then notifications}
+				{#each notifications as notification}
+					<Notification data={notification} small />
+				{/each}
+			{/await}
+		</div>
+	</div>
 </template>
