c6fbdba81a38253b6217c171db72fb53386bbcdc
blueb <ihateblueb@proton.me>
2024-09-25 04:20:22 -0400

    ApActorService optional cache

--- packages/backend/src/services/ap/ApActorService.ts
+++ packages/backend/src/services/ap/ApActorService.ts
@@ -3,12 +3,15 @@ import db from '../../utils/database.js';
 import ApResolverService from './ApResolverService.js';
 
 class ApActorService {
-	public async get(apId: string) {
+	public async get(apId: string, cache?: boolean) {
 		logger.debug('ap', 'getting actor by id ' + apId);
 
-		let actor = await db.getRepository('user').findOne({
-			where: { ap_id: apId }
-		});
+		let actor = await db
+			.getRepository('user')
+			.createQueryBuilder()
+			.where({ ap_id: apId })
+			.cache(cache)
+			.getOne();
 
 		if (actor) {
 			logger.debug('ap', 'actor ' + apId + ' found in database');
