35d0586a680f01d31fe3dbb2a45c6546065f3a69
blueb <ihateblueb@proton.me>
2024-10-05 10:25:16 -0400

    add more logger types

--- packages/backend/src/utils/logger.ts
+++ packages/backend/src/utils/logger.ts
@@ -7,8 +7,24 @@ class logger {
 		this.log('done', region, message);
 	}
 
+	public debug(region: string, message: string) {
+		this.log('debug', region, message);
+	}
+
 	public info(region: string, message: string) {
 		this.log('info', region, message);
+	}
+
+	public warn(region: string, message: string) {
+		this.log('warn', region, message);
+	}
+
+	public error(region: string, message: string) {
+		this.log('error', region, message);
+	}
+
+	public fatal(region: string, message: string) {
+		this.log('fatal', region, message);
 	}
 }
 
