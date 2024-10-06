9b1a19a7656d15daad200e7045f091759c05ba1f
blueb <ihateblueb@proton.me>
2024-10-06 12:30:56 -0400

    userpriv and config

---
+++ packages/backend/src/utils/config.ts
@@ -0,0 +1,14 @@
+import { parse } from 'ini';
+import { readFile } from 'node:fs/promises';
+
+let configText = await readFile(`../../config/production.ini`, {
+	encoding: 'utf-8'
+});
+
+configText = await readFile(`../../config/production.ini`, {
+	encoding: 'utf-8'
+});
+
+let config = parse(configText);
+
+export default config;
