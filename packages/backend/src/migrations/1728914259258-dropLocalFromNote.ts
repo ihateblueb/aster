5438e89f4105aa11d11205876f187dcf43bf6d18
blueb <ihateblueb@proton.me>
2024-10-14 10:03:53 -0400

    add migration to drop local from note

---
+++ packages/backend/src/migrations/1728914259258-dropLocalFromNote.ts
@@ -0,0 +1,14 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class DropLocalFromNote1728914259258 implements MigrationInterface {
+    name = 'DropLocalFromNote1728914259258'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "note" DROP COLUMN "local"`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "note" ADD "local" boolean NOT NULL DEFAULT false`);
+    }
+
+}
