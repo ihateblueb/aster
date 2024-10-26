fea0e3cea88cb73baff1a04f328b2ec5830a1118
blueb <ihateblueb@proton.me>
2024-10-26 09:02:51 -0400

    move test

--- packages/backend/tests/ap/user.test.ts
+++
@@ -1,5 +0,0 @@
-import config from '../../built/utils/config.js';
-
-let req = fetch(new URL(config.url).href + 'users/');
-
-test('ap user endpoint returns content type application/activity+json', () => {});
