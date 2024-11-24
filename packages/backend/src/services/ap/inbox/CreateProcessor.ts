a6e6ebfb19a598dc21781ee92f1eb6d3c7b4c12c
blueb <ihateblueb@proton.me>
2024-11-24 12:12:37 -0500

    allow activity Create Actor

--- packages/backend/src/services/ap/inbox/CreateProcessor.ts
+++ packages/backend/src/services/ap/inbox/CreateProcessor.ts
@@ -1,13 +1,22 @@
+import ApActorService from '../ApActorService.js';
 import ApNoteService from '../ApNoteService.js';
 
 class CreateProcessor {
 	public async process(body): Promise<boolean> {
 		console.log(body); // todo: remove
 
-		if (body.object.type !== 'Note') return false;
-		await ApNoteService.register(body.object);
-
-		return true;
+		if (body.object.type === 'Note') {
+			await ApNoteService.register(body.object);
+			return true;
+		} else if (
+			body.object.type === 'Person' ||
+			body.object.type === 'Service'
+		) {
+			await ApActorService.register(body.object);
+			return true;
+		} else {
+			return false;
+		}
 	}
 }
 
