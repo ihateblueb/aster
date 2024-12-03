2a6355e8ecd316a1cfc9488888598c0941e9c20f
blueb <ihateblueb@proton.me>
2024-12-02 21:28:51 -0500

    add: another relation (I GOTTA FIX THIS), frontend for repeated note

--- packages/frontend/src/lib/components/Note.svelte
+++ packages/frontend/src/lib/components/Note.svelte
@@ -1,4 +1,17 @@
-<script ✂prettier:content✂="CglpbXBvcnQgTm90ZUhlYWRlciBmcm9tICckbGliL2NvbXBvbmVudHMvTm90ZUhlYWRlci5zdmVsdGUnOwoJaW1wb3J0IE5vdGVGb290ZXIgZnJvbSAnJGxpYi9jb21wb25lbnRzL05vdGVGb290ZXIuc3ZlbHRlJzsKCWltcG9ydCBNZm0gZnJvbSAnJGxpYi9jb21wb25lbnRzL01mbS5zdmVsdGUnOwoJaW1wb3J0IEJ1dHRvbiBmcm9tICckbGliL2NvbXBvbmVudHMvQnV0dG9uLnN2ZWx0ZSc7CglpbXBvcnQgeyBJY29uUmVwZWF0IH0gZnJvbSAnQHRhYmxlci9pY29ucy1zdmVsdGUnOwoJaW1wb3J0IFRpbWUgZnJvbSAnJGxpYi9jb21wb25lbnRzL1RpbWUuc3ZlbHRlJzsKCWltcG9ydCBBdmF0YXIgZnJvbSAnJGxpYi9jb21wb25lbnRzL0F2YXRhci5zdmVsdGUnOwoJaW1wb3J0IFZpc2liaWxpdHkgZnJvbSAnJGxpYi9jb21wb25lbnRzL1Zpc2liaWxpdHkuc3ZlbHRlJzsKCglleHBvcnQgbGV0IG5vdGU7CgoJbGV0IGN3T3BlbiA9IGZhbHNlOwo=">{}</script>
+<script>
+	import NoteHeader from '$lib/components/NoteHeader.svelte';
+	import NoteFooter from '$lib/components/NoteFooter.svelte';
+	import Mfm from '$lib/components/Mfm.svelte';
+	import Button from '$lib/components/Button.svelte';
+	import { IconRepeat } from '@tabler/icons-svelte';
+	import Time from '$lib/components/Time.svelte';
+	import Avatar from '$lib/components/Avatar.svelte';
+	import Visibility from '$lib/components/Visibility.svelte';
+
+	export let note;
+
+	let cwOpen = false;
+</script>
 
 {#snippet renderNote(data)}
 	<NoteHeader note={data} />
@@ -8,7 +21,7 @@
 				<div class="cw">
 					<span>{data.cw}</span>
 					<button on:click={() => (cwOpen = !cwOpen)}
-					>{!cwOpen ? 'Show' : 'Hide'}</button
+						>{!cwOpen ? 'Show' : 'Hide'}</button
 					>
 				</div>
 				{#if cwOpen}
@@ -51,4 +64,96 @@
 	{/if}
 </article>
 
-<style lang="scss" scoped ✂prettier:content✂="CglhcnRpY2xlIHsKCQlwYWRkaW5nOiAxNnB4OwoJCXRyYW5zaXRpb246IDAuMXM7CgoJCS5yZXBlYXRIZWFkZXIgewoJCQlkaXNwbGF5OiBmbGV4OwoJCQlhbGlnbi1pdGVtczogY2VudGVyOwoKCQkJZ2FwOiAxMHB4OwoJCQltYXJnaW4tYm90dG9tOiAxMHB4OwoKCQkJLmxlZnQgewoJCQkJZGlzcGxheTogZmxleDsKCQkJCWFsaWduLWl0ZW1zOiBjZW50ZXI7CgkJCQlnYXA6IDEwcHg7CgkJCQlmbGV4LWdyb3c6IDE7CgoJCQkJYSB7CgkJCQkJZm9udC13ZWlnaHQ6IDYwMDsKCQkJCQljb2xvcjogdmFyKC0tdHgyKTsKCQkJCQl0ZXh0LWRlY29yYXRpb246IG5vbmU7CgkJCQl9CgkJCX0KCgkJCS5yaWdodCB7CgkJCQlkaXNwbGF5OiBmbGV4OwoJCQkJYWxpZ24taXRlbXM6IGNlbnRlcjsKCQkJCWdhcDogNXB4OwoJCQkJZm9udC1zaXplOiB2YXIoLS1mcy1zbSk7CgkJCX0KCQl9CgoJCS5jb250ZW50IHsKCQkJbWFyZ2luOiAxMHB4IDA7CgoJCQkuY3cgewoJCQkJZGlzcGxheTogZmxleDsKCQkJCWFsaWduLWl0ZW1zOiBjZW50ZXI7CgkJCQl3aWR0aDogMTAwJTsKCgkJCQljb2xvcjogdmFyKC0tYWMxKTsKCQkJCWZvbnQtd2VpZ2h0OiA1MDA7CgkJCQliYWNrZ3JvdW5kLWNvbG9yOiB2YXIoLS1hYzEtMjUpOwoJCQkJYm9yZGVyLXJhZGl1czogdmFyKC0tYnItbWQpOwoJCQkJcGFkZGluZzogNnB4IDEwcHg7CgoJCQkJYm94LXNoYWRvdzogdmFyKC0tZnVua3ktZWZmZWN0KTsKCQkJCWJveC1zaXppbmc6IGJvcmRlci1ib3g7CgkJCQltYXJnaW4tYm90dG9tOiAxMHB4OwoKCQkJCWJ1dHRvbiB7CgkJCQkJZGlzcGxheTogZmxleDsKCQkJCQlhbGlnbi1pdGVtczogY2VudGVyOwoJCQkJCWJveC1zaXppbmc6IGJvcmRlci1ib3g7CgoJCQkJCWJvcmRlcjogbm9uZTsKCQkJCQlib3JkZXItcmFkaXVzOiB2YXIoLS1ici1tZCk7CgkJCQkJcGFkZGluZzogNnB4IDEwcHg7CgoJCQkJCWZvbnQtZmFtaWx5OiB2YXIoLS1mb250KTsKCQkJCQlmb250LWZlYXR1cmUtc2V0dGluZ3M6IHZhcigtLWZvbnQtZmVhdHVyZXMpOwoJCQkJCWZvbnQtc2l6ZTogdmFyKC0tZnMtbWQpOwoJCQkJCWZvbnQtd2VpZ2h0OiA0MDA7CgkJCQkJdGV4dC1kZWNvcmF0aW9uOiBub25lOwoKCQkJCQl0cmFuc2l0aW9uOiAwLjFzLAoJCQkJCXdpZHRoIDBzLAoJCQkJCWhlaWdodCAwcywKCQkJCQlvdXRsaW5lIDBzOwoKCQkJCQljb2xvcjogdmFyKC0tdHgxKTsKCQkJCQliYWNrZ3JvdW5kLWNvbG9yOiB0cmFuc3BhcmVudDsKCgkJCQkJJjpob3ZlciB7CgkJCQkJCWNvbG9yOiB2YXIoLS10eDEpOwoJCQkJCQliYWNrZ3JvdW5kLWNvbG9yOiB2YXIoLS1hYzEtMjUpOwoJCQkJCQlib3gtc2hhZG93OiB2YXIoLS1mdW5reS1lZmZlY3QpOwoJCQkJCX0KCQkJCX0KCgkJCQlzcGFuIHsKCQkJCQlmbGV4LWdyb3c6IDE7CgkJCQl9CgkJCX0KCQl9CgoJCSY6aG92ZXIgewoJCQlib3JkZXItcmFkaXVzOiB2YXIoLS1ici1tZCk7CgkJCWJhY2tncm91bmQtY29sb3I6IHZhcigtLWJnMy0yNSk7CgkJfQoJfQo="></style>
+<style lang="scss" scoped>
+	article {
+		padding: 16px;
+		transition: 0.1s;
+
+		.repeatHeader {
+			display: flex;
+			align-items: center;
+
+			gap: 10px;
+			margin-bottom: 10px;
+
+			.left {
+				display: flex;
+				align-items: center;
+				gap: 10px;
+				flex-grow: 1;
+
+				a {
+					font-weight: 600;
+					color: var(--tx2);
+					text-decoration: none;
+				}
+			}
+
+			.right {
+				display: flex;
+				align-items: center;
+				gap: 5px;
+				font-size: var(--fs-sm);
+			}
+		}
+
+		.content {
+			margin: 10px 0;
+
+			.cw {
+				display: flex;
+				align-items: center;
+				width: 100%;
+
+				color: var(--ac1);
+				font-weight: 500;
+				background-color: var(--ac1-25);
+				border-radius: var(--br-md);
+				padding: 6px 10px;
+
+				box-shadow: var(--funky-effect);
+				box-sizing: border-box;
+				margin-bottom: 10px;
+
+				button {
+					display: flex;
+					align-items: center;
+					box-sizing: border-box;
+
+					border: none;
+					border-radius: var(--br-md);
+					padding: 6px 10px;
+
+					font-family: var(--font);
+					font-feature-settings: var(--font-features);
+					font-size: var(--fs-md);
+					font-weight: 400;
+					text-decoration: none;
+
+					transition:
+						0.1s,
+						width 0s,
+						height 0s,
+						outline 0s;
+
+					color: var(--tx1);
+					background-color: transparent;
+
+					&:hover {
+						color: var(--tx1);
+						background-color: var(--ac1-25);
+						box-shadow: var(--funky-effect);
+					}
+				}
+				span {
+					flex-grow: 1;
+				}
+			}
+		}
+
+		&:hover {
+			border-radius: var(--br-md);
+			background-color: var(--bg3-25);
+		}
+	}
+</style>
