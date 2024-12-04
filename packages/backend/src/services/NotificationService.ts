5fdaca1132e5e93c3bad059523fbbfc396c92d8d
blueb <ihateblueb@proton.me>
2024-12-03 22:26:27 -0500

    scaffolding for NotificationService

---
+++ packages/backend/src/services/NotificationService.ts
@@ -0,0 +1,7 @@
+class NotificationService {
+	public get(where: object) {}
+	public getMany(where: object) {}
+	public create(to: string, from: string, type: string, note?: string, user?: string) {}
+}
+
+export default new NotificationService();
