53b1d72facbe35382a9bf0f7aef3ff8e250508c7
blueb <ihateblueb@proton.me>
2025-01-19 00:13:21 -0500

    clean up: remove half-assed search and format

--- packages/backend/src/migrations/1737240545396-migration.ts
+++ packages/backend/src/migrations/1737240545396-migration.ts
@@ -1,16 +1,23 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class Migration1737240545396 implements MigrationInterface {
-    name = 'Migration1737240545396'
+	name = 'Migration1737240545396';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "drive_file" ADD "userId" character varying`);
-        await queryRunner.query(`ALTER TABLE "drive_file" ADD CONSTRAINT "FK_860fa6f6c7df5bb887249fba22e" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
-    }
+	public async up(queryRunner: QueryRunner): Promise<void> {
+		await queryRunner.query(
+			`ALTER TABLE "drive_file" ADD "userId" character varying`
+		);
+		await queryRunner.query(
+			`ALTER TABLE "drive_file" ADD CONSTRAINT "FK_860fa6f6c7df5bb887249fba22e" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE NO ACTION`
+		);
+	}
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "drive_file" DROP CONSTRAINT "FK_860fa6f6c7df5bb887249fba22e"`);
-        await queryRunner.query(`ALTER TABLE "drive_file" DROP COLUMN "userId"`);
-    }
-
+	public async down(queryRunner: QueryRunner): Promise<void> {
+		await queryRunner.query(
+			`ALTER TABLE "drive_file" DROP CONSTRAINT "FK_860fa6f6c7df5bb887249fba22e"`
+		);
+		await queryRunner.query(
+			`ALTER TABLE "drive_file" DROP COLUMN "userId"`
+		);
+	}
 }
--- packages/backend/src/migrations/1737258260284-migration.ts
+++ packages/backend/src/migrations/1737258260284-migration.ts
@@ -1,14 +1,17 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class Migration1737258260284 implements MigrationInterface {
-    name = 'Migration1737258260284'
+	name = 'Migration1737258260284';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "note" RENAME COLUMN "origin" TO "receivedAt"`);
-    }
+	public async up(queryRunner: QueryRunner): Promise<void> {
+		await queryRunner.query(
+			`ALTER TABLE "note" RENAME COLUMN "origin" TO "receivedAt"`
+		);
+	}
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "note" RENAME COLUMN "receivedAt" TO "origin"`);
-    }
-
+	public async down(queryRunner: QueryRunner): Promise<void> {
+		await queryRunner.query(
+			`ALTER TABLE "note" RENAME COLUMN "receivedAt" TO "origin"`
+		);
+	}
 }
