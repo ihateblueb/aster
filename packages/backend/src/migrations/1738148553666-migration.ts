e03959f0f1993b7fdc50dd5c15b4562fdcc58d48
ihateblueb <ihateblueb@proton.me>
2025-01-29 06:03:22 -0500

    backend: add jsonb array "metadata" column for profile fields

---
+++ packages/backend/src/migrations/1738148553666-migration.ts
@@ -0,0 +1,16 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class Migration1738148553666 implements MigrationInterface {
+    name = 'Migration1738148553666'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "metadata"`);
+        await queryRunner.query(`ALTER TABLE "user" ADD "metadata" jsonb array`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "metadata"`);
+        await queryRunner.query(`ALTER TABLE "user" ADD "metadata" jsonb`);
+    }
+
+}
