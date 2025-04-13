4e87d8c93f1909ccaacd7c2b2a0aeb65f53054a2
ihateblueb <ihateblueb@proton.me>
2025-04-13 12:38:54 -0400

    backend: i forgot to use this type alias, fixed

--- packages/backend/src/services/TimeService.ts
+++ packages/backend/src/services/TimeService.ts
@@ -1,11 +1,11 @@
 type UnitOfTime = number;
 
 class TimeService {
-	public second = 1000;
-	public minute = 60 * this.second;
-	public hour = 60 * this.minute;
-	public day = 24 * this.hour;
-	public week = 7 * this.day;
+	public second: UnitOfTime = 1000;
+	public minute: UnitOfTime = 60 * this.second;
+	public hour: UnitOfTime = 60 * this.minute;
+	public day: UnitOfTime = 24 * this.hour;
+	public week: UnitOfTime = 7 * this.day;
 
 	public isTimeAgo(time: Date, distance: UnitOfTime): boolean {
 		const sinceEpoch = Date.now() - time.getTime();
