f5c95c46a570057742ab427661c0158f7c2903a3
ihateblueb <ihateblueb@proton.me>
2025-04-30 12:46:05 -0400

    backend: sample test for webfinger service so i can work on it later for the search service

---
+++ packages/backend/src/tests/services/WebfingerService.test.ts
@@ -0,0 +1,31 @@
+import { expect, test } from "vitest";
+import WebfingerService from "../../services/WebfingerService";
+
+await WebfingerService.lookup("@blueb@remlit.site")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+test('generate returns value', () => {
+	expect(id !== undefined);
+});
